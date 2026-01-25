9bc238bbe685a198a2ebc11670c13ae550dbf2b2
Blagoj Ramadanov <blaze.ramadanov@gmail.com>
2026-01-25 20:58:40 +0100

    fetching

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+import { fetchJoke } from "./fetch";
 import "./styles/main.scss";
+
+const jokeText = document.querySelector(".current-joke__text");
+const newJokeBtn = document.querySelector(".current-joke__create");
+
+async function loadJoke() {
+  jokeText.innerText = "Loading...";
+
+  const joke = await fetchJoke();
+  jokeText.innerText = joke;
+}
+
+newJokeBtn.addEventListener("click", loadJoke);
