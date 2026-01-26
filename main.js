5e1664b2f11dcdbcca7aeeb0c7f52a8711965c7f
Blagoj Ramadanov <blaze.ramadanov@gmail.com>
2026-01-26 20:14:38 +0100

    save joke and fix design saved list

--- main.js
+++ main.js
@@ -3,12 +3,48 @@ import "./styles/main.scss";
 
 const jokeText = document.querySelector(".current-joke__text");
 const newJokeBtn = document.querySelector(".current-joke__create");
+const saveJokeBtn = document.querySelector(".current-joke__save");
+const savedJokesList = document.querySelector(".saved-jokes__list");
 
 async function loadJoke() {
-  jokeText.innerText = "Loading...";
-
   const joke = await fetchJoke();
   jokeText.innerText = joke;
+  saveJokeBtn.classList.remove("current-joke__save--nonscreen");
 }
 
 newJokeBtn.addEventListener("click", loadJoke);
+
+saveJokeBtn.addEventListener("click", () => {
+  const joke = jokeText.innerText;
+
+  const div = document.createElement("div");
+  div.className = "saved-joke";
+  div.innerHTML = `
+    <div class="saved-joke__text">${joke}</div>
+    <button class="saved-joke__remove">
+      <svg
+        data-slot="icon"
+        fill="none"
+        stroke-width="1.5"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        class="current-joke__delete-icon"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+        ></path>
+      </svg>
+    </button>
+  `;
+
+  savedJokesList.appendChild(div);
+
+  const removeBtn = div.querySelector(".saved-joke__remove");
+  removeBtn.addEventListener("click", () => {
+    div.remove();
+  });
+});
